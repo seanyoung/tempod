18ea3f0a22455cd778d2f820cda83b16c08c4dc1
Sean Young <sean@mess.org>
2014-12-03 16:56:40 +0000

    use gnu stuff

--- make.sh
+++ make.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gcc -o tempod tempod.c -O3 -Wall -lbluetooth -levent -DGIT_COMMIT=\"$(git log -1 --oneline | cut -f1 '-d ')\"
+gcc -o tempod tempod.c -O3 -Wall -lbluetooth -levent -DGIT_COMMIT=\"$(git log -1 --oneline | cut -f1 '-d ')\" -D_GNU_SOURCE
